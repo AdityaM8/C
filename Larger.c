5bfa3cbf60001a18bc61e35e8b273c41f9638d2b
Aditya Milind Mate <53221701+AdityaM8@users.noreply.github.com>
2020-01-22 22:41:37 +0530

    Add files via upload

---
+++ Larger.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int a,b;   
+     printf("Enter two numbers");  
+    scanf("%d %d",&a,&b);  
+    if(a>b)  
+    {  
+        printf("%d is larger",a);  
+    }  
+   else if(b>a)  
+    {  
+        printf("%d is larger",b);  
+getch();
+}
+}
---
+++ evemodd.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include<conio.h> 
+ void main()  
+{  
+    int n=0;    
+printf("enter a number:");    
+scanf("%d",&n);     
+if(n%2==0)
+{    
+printf("%d is even",n);    
+}    
+else
+{    
+printf("%d is odd",n);    
+}     
+    getch();
+    }
