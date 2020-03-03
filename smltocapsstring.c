1173ed570fcea90debea3806c1f9645826b21edf
Aditya Milind Mate <53221701+AdityaM8@users.noreply.github.com>
2020-03-03 18:32:58 +0530

    Add files via upload

---
+++ smltocapsstring.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define MAX 20
+int main()
+{
+	char str[MAX]; int i=0,c=0;
+	printf("\n Enter srting:");
+	scanf("%s",str);
+	while(str[i]!='\0')
+	{
+		if(str[i]>='a' && str[i]<='z')
+		{
+			str[i]=str[i]+('A'-'a');
+			i++;
+		}
+			
+	}
+	printf("%s",str);
+	return(0);
+}
