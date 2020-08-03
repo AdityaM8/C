4303bcebae79042c02bc6dcc35d0f339bae8dabc
Aditya Milind Mate <53221701+AdityaM8@users.noreply.github.com>
2020-08-03 15:01:46 +0530

    Add files via upload

---
+++ he1seats.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+int t, n;
+scanf("%d",&t);
+while(t--)
+{
+scanf("%d",&n);
+
+switch(n%12)
+{
+case 1: printf("%d WS\n",n+11);
+break;
+case 2: printf("%d MS\n",n+9);
+break;
+case 3: printf("%d AS\n",n+7);
+break;
+case 4: printf("%d AS\n",n+5);
+break;
+case 5: printf("%d MS\n",n+3);
+break;
+case 6: printf("%d WS\n",n+1);
+break;
+case 7: printf("%d WS\n",n-1);
+break;
+case 8: printf("%d MS\n",n-3);
+break;
+case 9: printf("%d AS\n",n-5);
+break;
+case 10: printf("%d AS\n",n-7);
+break;
+case 11: printf("%d MS\n",n-9);
+break;
+case 0: printf("%d WS\n",n-11);
+break;
+}
+}
+return 0;
+}
